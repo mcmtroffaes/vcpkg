5de0a155e7e30033537a2944767385a886bfbc6b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-10-29 08:47:56 +0000

    [openssl] also add ws2_32 and link to upstream bug report in cmake

--- ports/openssl/vcpkg-cmake-wrapper.cmake
+++ ports/openssl/vcpkg-cmake-wrapper.cmake
@@ -1,5 +1,6 @@
 _find_package(${ARGS})
 
+# workaround for https://gitlab.kitware.com/cmake/cmake/-/issues/19263
 if(WIN32 AND NOT CYGWIN)
-  list(APPEND OPENSSL_LIBRARIES Crypt32)
+  list(APPEND OPENSSL_LIBRARIES Crypt32 ws2_32)
 endif()
